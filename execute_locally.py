a7206e6f429c51c0defb0f47ebe37ec44012c710
Aster Santana <50426897+astersantana@users.noreply.github.com>
2022-01-05 14:19:11 -0300

    Edited execute_locally.py

--- execute_locally.py
+++ execute_locally.py
@@ -1,4 +1,4 @@
 from mip_me import schemas
 
-input_schema = schemas.input_schema
-print(input_schema.primary_key_fields)
+primary_keys = schemas.input_schema.primary_key_fields
+print(primary_keys)
