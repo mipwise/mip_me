bf96e745ce05d1d6ebb4d73516d319060b0f237d
Aster Santana <50426897+astersantana@users.noreply.github.com>
2022-01-06 21:20:21 -0300

    Imported input and output schemas in `__init__.py`

--- execute_locally.py
+++ execute_locally.py
@@ -1,4 +1,5 @@
-from mip_me import schemas
-input_schema = schemas.input_schema
-primary_keys = input_schema.primary_key_fields
-print(primary_keys)
+from mip_me import input_schema
+path = "test_mip_me/data/inputs"
+dat = input_schema.csv.create_pan_dat(path)
+print(dat)
+
