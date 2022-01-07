0e7ea63f674b9afcfffb79addac2b49b538784e8
Aster Santana <50426897+astersantana@users.noreply.github.com>
2022-01-07 18:59:32 -0300

    Updated execute_locally.py to write data back to a directory

--- execute_locally.py
+++ execute_locally.py
@@ -1,7 +1,7 @@
 from mip_me import input_schema
 from mip_me.action_update_food_cost import update_food_cost_solve
-path = "test_mip_me/data/inputs"
-dat = input_schema.csv.create_pan_dat(path)
-print('Before:\n', dat.foods)
+input_path = "test_mip_me/data/inputs"
+output_path = "test_mip_me/data/inputs/testing_data.xlsx"
+dat = input_schema.csv.create_pan_dat(input_path)
 dat = update_food_cost_solve(dat)
-print('After:\n', dat.foods)
+input_schema.xls.write_file(dat, output_path)
