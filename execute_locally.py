ed6ca940d659267ac84449773f8820d242a5359b
Aster Santana <50426897+astersantana@users.noreply.github.com>
2022-01-06 21:59:57 -0300

    Implemented the Update Food Cost action

--- execute_locally.py
+++ execute_locally.py
@@ -1,5 +1,7 @@
 from mip_me import input_schema
+from mip_me.action_update_food_cost import update_food_cost_solve
 path = "test_mip_me/data/inputs"
 dat = input_schema.csv.create_pan_dat(path)
-print(dat)
-
+print('Before:\n', dat.foods)
+dat = update_food_cost_solve(dat)
+print('After:\n', dat.foods)
