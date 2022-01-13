c3e316d9f56d7e8a96e5439190b0692891f62dd6
Aster Santana <50426897+astersantana@users.noreply.github.com>
2022-01-13 17:46:15 -0300

    Made execute_locally.py more robust

--- test_mip_me/execute_locally.py
+++ test_mip_me/execute_locally.py
@@ -1,9 +1,59 @@
 from mip_me import input_schema, output_schema
-from mip_me import build_report_solve
+from mip_me import update_food_cost_solve
+from mip_me import solve
+from mip_me import report_builder_solve
 
-input_path = "data/inputs"
-output_path = "data/outputs"
-dat = input_schema.csv.create_pan_dat(input_path)
-sln = output_schema.csv.create_pan_dat(output_path)
-sln = build_report_solve(dat, sln)
-output_schema.csv.write_directory(sln, output_path)
+import os
+import inspect
+
+
+def _this_directory():
+    return os.path.dirname(os.path.realpath(os.path.abspath(inspect.getsourcefile(_this_directory))))
+
+
+def read_xls_data(schema, data):
+    path = os.path.join(_this_directory(), "data", data)
+    assert os.path.exists(path), f"bad path {path}"
+    print(f'Reading data from: {data}')
+    return schema.xls.create_pan_dat(path)
+
+
+def read_csv_data(schema, data):
+    path = os.path.join(_this_directory(), "data", data)
+    assert os.path.exists(path), f"bad path {path}"
+    print(f'Reading data from: {data}')
+    return schema.csv.create_pan_dat(path)
+
+
+def write_csv_data(dat_sln, schema, solution_dir):
+    path = os.path.join(_this_directory(), "data", solution_dir)
+    assert os.path.exists(path), f"bad path {path}"
+    print(f'Writing data back to: {solution_dir}')
+    return schema.csv.write_directory(dat_sln, path)
+
+
+engine = {0: 'Data Ingestion',
+          1: 'Action Update Food Cost',
+          2: 'Main Solve',
+          3: 'Action Report Builder'}[3]
+
+if engine == 'Data Ingestion':
+    dat = read_xls_data(input_schema, 'testing_data.xlsx')
+    write_csv_data(dat, input_schema, 'inputs')
+elif engine == 'Action Update Food Cost':
+    dat = read_csv_data(input_schema, 'inputs')
+    dat = update_food_cost_solve(dat)
+    write_csv_data(dat, input_schema, 'inputs')
+elif engine == 'Main Solve':
+    dat = read_csv_data(input_schema, 'inputs')
+    sln = solve(dat)
+    write_csv_data(sln, output_schema, 'outputs')
+elif engine == 'Action Report Builder':
+    dat = read_csv_data(input_schema, 'inputs')
+    sln = read_csv_data(output_schema, 'outputs')
+    sln = report_builder_solve(dat, sln)
+    write_csv_data(sln, output_schema, 'outputs')
+else:
+    raise ValueError('Bad engine')
+
+
