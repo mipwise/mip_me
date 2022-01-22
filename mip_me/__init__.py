9c4459f9472f31de433409505e1962ad3655c8e1
Aster Santana <50426897+astersantana@users.noreply.github.com>
2022-01-21 23:11:46 -0300

    Renamed tool-tip to tooltip in the config files

--- mip_me/__init__.py
+++ mip_me/__init__.py
@@ -8,11 +8,11 @@ actions_config = {
     'Update Food Cost': {
         'schema': 'input',
         'engine': update_food_cost_solve,
-        'tool_tip': "Update the food cost by the factor entered in the 'Food Cost Multiplier' parameter"},
+        'tooltip': "Update the food cost by the factor entered in the 'Food Cost Multiplier' parameter"},
     'Report Builder': {
         'schema': 'output',
         'engine': report_builder_solve,
-        'tool_tip': "Reads the output from the main engine and populate the report tables"}
+        'tooltip': "Reads the output from the main engine and populate the report tables"}
     }
 
 input_tables_config = {
@@ -47,7 +47,7 @@ parameters_config = {
     'hidden': [],
     'categories': dict(),
     'order': ['Food Cost', 'Food Portions'],
-    'tool_tips': {
+    'tooltips': {
         'Food Portions': "Whether fractional portions may compose the diet or only whole portions must be used",
         'Food Cost Multiplier': "The factor used by the Update Food Cost action to change food cost"
         }
